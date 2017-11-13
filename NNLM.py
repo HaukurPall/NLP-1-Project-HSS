a9e7ac839f947a299e7ba7b07cfa3f31b08fe788
stianste <stianste@stud.ntnu.no>
2017-11-13 13:02:25 +0100

    Add ngram helper

--- NNLM.py
+++ NNLM.py
@@ -5,12 +5,18 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 from data_reader import DataReader
+from ngram_helper import extract_list_of_ngrams
 
-word_vector_dimention = 50
+word_vector_dimension = 50
 
-traing_data = DataReader("data/penn/train.txt")
+training_data = DataReader("data/penn/train.txt", read_limit=10)
 
-word_to_index = traing_data.get_word_indexes()
+word_to_index = training_data.get_word_indexes()
 vocab_size = len(word_to_index)
 
-print(vocab_size)
+embeds = nn.Embedding(vocab_size, word_vector_dimension)
+
+words = training_data.get_words()
+trigrams = extract_list_of_ngrams(words, 3)
+
+for trigram in trigrams: print(trigram)
--- data_reader.py
+++ data_reader.py
@@ -1,13 +1,18 @@
 from collections import defaultdict
+from math import inf
 
 class DataReader:
-  def __init__(self, filepath):
+  def __init__(self, filepath, read_limit=inf):
     self.words = []
     with open(filepath, "r") as f:
-      for line in f.readlines():
+      for line_number, line in enumerate(f.readlines()):
         for word in line.split():
           self.words.append(word)
+        if line_number > read_limit: break
 
+
+  def get_words(self):
+    return self.words
 
   def get_word_indexes(self):
     # Returns a dictionary containing all the words in the vocabulary
