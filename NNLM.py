c667f675ad8f4b1aaba0f91e655b2ec65850ceb8
stianste <stianste@stud.ntnu.no>
2017-11-16 19:39:07 +0100

    Fix Git problem
    This is several commits in one. Some highlights include adding
    perplexity measure, refactoring, adding the pre-computed Standford
    embeddings and move calculations over to GPU.

--- NNLM.py
+++ NNLM.py
@@ -4,41 +4,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from data_reader import DataReader
-from ngram_helper import extract_list_of_ngrams
-
-from math import inf
-
+from data_reader import get_pretrained_embeddings
 import time
 
 # Constants
-WORD_EMBEDDINGS_DIMENSION = 40
-NGRAM_SIZE = 3
-CONTEXT_SIZE = NGRAM_SIZE - 1
+WORD_EMBEDDINGS_DIMENSION = 50
 LEARNING_RATE = 0.001
 NUMBER_OF_TRAINING_EPOCHS = 100
-READ_LIMIT = 10
-
-training_data = DataReader("data/penn/train.txt", read_limit=READ_LIMIT)
-test_data = DataReader("data/penn/test.txt", read_limit=READ_LIMIT)
-
-word_to_index = training_data.get_word_indexes()
-vocab_size = len(word_to_index)
-
-words = training_data.get_words()
-trigrams = extract_list_of_ngrams(words, NGRAM_SIZE)
-
-print("Done reading data and creating trigrams")
+LOSS_THRESHOLD = 0.01
 
 class NGramLanguageModeler(nn.Module):
 
   # Taken straight out of the example. We might want to make our own adjustments here
-
-  def __init__(self, vocab_size, embedding_dim, context_size):
+  def __init__(self, vocab_size, embedding_dim, context_size, word_embeddings):
     super(NGramLanguageModeler, self).__init__()
     self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-    self.linear1 = nn.Linear(context_size * embedding_dim, 128)
-    self.linear2 = nn.Linear(128, vocab_size)
+    self.embeddings.weight.data.copy_(word_embeddings)
+    self.embeddings = self.embeddings.cuda()
+    self.embeddings.weight.requires_grad = False # Do not train the pre-calculated embeddings
+    self.linear1 = nn.Linear(context_size * embedding_dim, 64)
+    self.linear1 = self.linear1.cuda()
+    self.linear2 = nn.Linear(64, vocab_size)
+    self.linear2 = self.linear2.cuda()
 
   def forward(self, inputs):
     embeds = self.embeddings(inputs).view((1, -1))
@@ -47,21 +34,35 @@ class NGramLanguageModeler(nn.Module):
     log_probs = F.log_softmax(out)
     return log_probs
 
-def train_model():
+def get_target_else_unknown(lookuptable, target):
+  # TODO: Handle cyclic import so we can import this from train_and_evaluate 
+  # instead of duplicate code
+  if target in lookuptable:
+    return lookuptable[target]
+  else:
+    return lookuptable["unknown"]
 
-  loss_function = nn.NLLLoss()
-  model = NGramLanguageModeler(vocab_size, WORD_EMBEDDINGS_DIMENSION, CONTEXT_SIZE)
-  optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE)
+def train_model(trigrams, vocab_size, CONTEXT_SIZE, word_to_index, word_embeddings):
+
+  start_time = time.time()
+
+  total_losses = []
+  loss_function = nn.NLLLoss().cuda()
+  model = NGramLanguageModeler(vocab_size, WORD_EMBEDDINGS_DIMENSION, CONTEXT_SIZE, word_embeddings)
+  optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=LEARNING_RATE)
 
   for epoch in range(NUMBER_OF_TRAINING_EPOCHS):
     print("epoch:", epoch)
-    total_loss = torch.Tensor([0])
-    for context, target in trigrams:
+    total_loss = torch.Tensor([0]).cuda()
+    for i, (context, target) in enumerate(trigrams):
+      if i % 1000 == 1:
+        print(str(i) + "/" + str(len(trigrams)), "Epoch is ", i/ len(trigrams) * 100, "% done", context, target )
+        print("Current runtime", time.time() - start_time)
 
       # Step 1. Prepare the inputs to be passed to the model (i.e, turn the words
       # into integer indices and wrap them in variables)
-      context_indexes = [word_to_index[w] for w in context]
-      context_var = autograd.Variable(torch.LongTensor(context_indexes))
+      context_indexes = [get_target_else_unknown(word_to_index, w) for w in context]
+      context_var = autograd.Variable(torch.LongTensor(context_indexes).cuda())
 
       # Step 2. Recall that torch *accumulates* gradients. Before passing in a
       # new instance, you need to zero out the gradients from the old
@@ -74,56 +75,22 @@ def train_model():
 
       # Step 4. Compute your loss function. (Again, Torch wants the target
       # word wrapped in a variable)
+      target_index = get_target_else_unknown(word_to_index, target)
+
       loss = loss_function(log_probs, autograd.Variable(
-          torch.LongTensor([word_to_index[target]])))
+          torch.LongTensor([target_index]).cuda()))
 
       # Step 5. Do the backward pass and update the gradient
       loss.backward()
       optimizer.step()
 
-  return model
+      total_loss += loss.data
 
-def get_max_value_index(tensor):
-  # this is probably not necessary, but I can't figure out how to get the index of the max element of a tensor.
-  best_value = -inf
-  best_index = -1
-  for i, value in enumerate(tensor.data[0]):
-    if value > best_value:
-      best_value = value
-      best_index = i
+    total_losses.append(total_loss)
+    if len(total_losses) > 1:
+      l = abs((total_losses[-1] - total_losses[-2])[0])
+      print("Current difference in loss:", l)
+      if l < LOSS_THRESHOLD:
+        break
 
-  return best_index
-
-def evaulate_model(model):
-  test_words = test_data.get_words()
-
-  test_trigrams = extract_list_of_ngrams(test_words, NGRAM_SIZE)
-
-  correct_predictions = 0
-  total_predictions = 0
-
-  for context, target in test_trigrams:
-    context_indexes = []
-    for w in context:
-      # Handle words we haven't seen in the training data as unkown
-      context_indexes.append(word_to_index[w] if w in training_data.get_vocabulary() else word_to_index["<unk>"])
-
-    context_var = autograd.Variable(torch.LongTensor(context_indexes))
-
-    log_probs = model(context_var)
-
-    predicted_word_index = get_max_value_index(log_probs)
-    assert predicted_word_index != -1, "The index of the predicted word was -1"
-
-    if predicted_word_index == word_to_index[target]:
-      correct_predictions += 1
-
-    total_predictions += 1
-
-  print("Correct predictions: ", correct_predictions/total_predictions)
-
-start_time = time.time()
-trained_model = train_model()
-print("Training time: ", time.time() - start_time)
-
-evaulate_model(trained_model)
+  return model.cuda()
---
+++ train_and_evaluate_model.py
@@ -0,0 +1,112 @@
+from NNLM import train_model
+from data_reader import DataReader, get_pretrained_word_indexes, get_pretrained_embeddings
+from ngram_helper import extract_list_of_ngrams
+from math import inf, pow, log
+
+import torch
+import torch.autograd as autograd
+
+import time
+
+# Constants
+READ_LIMIT = 50 # Currently still very slow since we still get all 400000 word embeddings
+NGRAM_SIZE = 5
+CONTEXT_SIZE = NGRAM_SIZE - 1
+
+
+def get_max_value_and_index(tensor):
+  # this is probably not necessary,
+  # but I can't figure out how to get the index of the max element of a tensor.
+  best_value = -inf
+  best_index = -1
+  for i, value in enumerate(tensor.data[0]):
+    if value > best_value:
+      best_value = value
+      best_index = i
+
+  return best_index, best_value
+
+def calculate_perplexity(sentence, model):
+
+  def perplexity(log_probability, n):
+    if 10 ** log_probability == 0:
+      return inf # TODO: Figure out how to deal with very low probabilities
+    return pow(10 ** log_probability, -(1/n))
+
+  sentence = sentence.split()
+  trigrams = extract_list_of_ngrams(sentence, NGRAM_SIZE)
+  probability_of_sentence = 0
+
+  for context, target in trigrams:
+    context_indexes = [get_target_else_unknown(word_to_index, w) for w in context]
+    context_var = autograd.Variable(torch.LongTensor(context_indexes).cuda())
+
+    log_probs = model(context_var)
+    probability = get_max_value_and_index(log_probs)[1]
+    probability_of_sentence += probability
+
+  return perplexity(probability_of_sentence, len(sentence))
+
+def calculate_average_perplexity(sentences, model):
+  total_perplexity = 0
+
+  for sentence in sentences:
+    total_perplexity += calculate_perplexity(sentence, model)
+
+  return total_perplexity / len(sentences)
+
+def get_target_else_unknown(lookuptable, target):
+  if target in lookuptable:
+    return lookuptable[target]
+
+  else:
+    return lookuptable["unknown"]
+
+def evaluate_model(model, test_data):
+  test_words = test_data.get_words()
+
+  test_trigrams = extract_list_of_ngrams(test_words, NGRAM_SIZE)
+
+  correct_predictions = 0
+  total_predictions = 0
+
+  for context, target in test_trigrams:
+    context_indexes = [get_target_else_unknown(word_to_index, w) for w in context]
+
+    context_var = autograd.Variable(torch.LongTensor(context_indexes).cuda())
+
+    log_probs = model(context_var)
+
+    predicted_word_index = get_max_value_and_index(log_probs)[0]
+    assert predicted_word_index != -1, "The index of the predicted word was -1"
+
+    if predicted_word_index == get_target_else_unknown(word_to_index, target):
+      correct_predictions += 1
+
+    total_predictions += 1
+
+  print("Correct predictions:", correct_predictions/total_predictions)
+  print("Correct predictions:", correct_predictions, "out of", total_predictions)
+
+  average_perplexity = calculate_average_perplexity(training_data.get_sentences(), model)
+  print("Average perplexity:", average_perplexity)
+
+def main():
+  training_data = DataReader("data/penn/train.txt", read_limit=READ_LIMIT)
+
+  predefined_vocabulary, word_embeddings = get_pretrained_embeddings()
+  vocab_size = len(predefined_vocabulary)
+  word_to_index = get_pretrained_word_indexes()
+
+  words = training_data.get_words()
+  trigrams = extract_list_of_ngrams(words, NGRAM_SIZE)
+  print("Done reading data and creating trigrams")
+
+  start_time = time.time()
+  trained_model = train_model(trigrams, vocab_size, CONTEXT_SIZE, word_to_index, word_embeddings)
+  print("Training time: ", time.time() - start_time)
+
+  test_data = DataReader("data/penn/test.txt", read_limit=READ_LIMIT)
+  evaluate_model(trained_model, test_data)
+
+main()
